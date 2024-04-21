8ed113e1bd14b2cbcf162709431a31901967d97d
Alex Hewitt <alexhewitt.j@gmail.com>
2024-04-21 15:18:54 -0500

    restructure again

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
-const express = require('express')
 const config = require('./utils/config')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+const app = require('./app')
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
