90bd1a9d9043687d2c3661f68ce55fb09d55bf99
Alex Hewitt <alexhewitt.j@gmail.com>
2024-04-21 15:57:33 -0500

    refactor final

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api', blogsRouter)
 
 module.exports = app
